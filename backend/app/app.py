53bc6a724ad3ea364db8c97d3718cfb71d4dafae
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-05-03 17:03:34 +0200

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 from app.config import Config
 from app.auth.routes import auth_bp
 from app.github.routes import github_bp
+from app.profile.routes import profile_bp
 
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    jwt.init_app(app)
 
     CORS(
         app,
@@ -28,9 +33,9 @@ def create_app():
     )
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
+    app.register_blueprint(profile_bp, url_prefix="/api/profile")
     app.register_blueprint(github_bp, url_prefix="/api")
 
     return app
-
 
 app = create_app()

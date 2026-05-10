f6e149f918ac324e1c4ac8c7e1d04b2f69768c7b
LUCA-PYTHON <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-05-10 20:52:13 +0200

    Fixed badge svg and userprofile back button

--- frontend/src/components/icons/svgs.jsx
+++ frontend/src/components/icons/svgs.jsx
@@ -5,7 +5,8 @@ import {
   RepoIcon,
   PersonIcon,
   GearIcon,
-  ProjectIcon
+  ProjectIcon,
+  TrophyIcon
 } from '@primer/octicons-react';
 
 const ICON_SIZE = 16;
@@ -15,15 +16,15 @@ const FORK_COLOR = '#79c0ff';
 const ISSUE_COLOR = '#ef4444';
 
 export function SvgStar(props) {
-  return (<StarIcon size={ICON_SIZE} color={STAR_COLOR} {...props} />);
+  return <StarIcon size={ICON_SIZE} color={STAR_COLOR} {...props} />;
 }
 
 export function SvgFork(props) {
-  return (<RepoForkedIcon size={ICON_SIZE} color={FORK_COLOR} {...props} />);
+  return <RepoForkedIcon size={ICON_SIZE} color={FORK_COLOR} {...props} />;
 }
 
 export function SvgOpenIssue(props) {
-  return (<IssueOpenedIcon size={ICON_SIZE} color={ISSUE_COLOR} {...props} />);
+  return <IssueOpenedIcon size={ICON_SIZE} color={ISSUE_COLOR} {...props} />;
 }
 
 export const SvgIssue = SvgOpenIssue;
@@ -42,4 +43,8 @@ export function SvgSettings(props) {
 
 export function SvgProducts(props) {
   return <ProjectIcon size={ICON_SIZE} {...props} />;
+}
+
+export function SvgBadges(props) {
+  return <TrophyIcon size={ICON_SIZE} {...props} />;
 }
